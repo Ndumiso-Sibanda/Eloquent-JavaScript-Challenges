392d8a9ccb4b568080d6f73fb25c77973556508e
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-28 09:50:14 +0200

    Added method to subtract one vector from another

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 class Vec {
-    constructor(x, y) {
+ constructor(x, y) {
       this.x = x;
       this.y = y;
     }
  plus(other) {
-    return new Vec(this.x + other.x, this.y + other.y);
+    return new 
+      Vec(this.x + other.x, this.y + other.y);
     }
+ minus(other) {
+     return new
+      Vec(this.x - other.x, this.y - other.y);
+    }   
 }
