ba15ebb4d6de303a51f4914dcf0f0c71dab22e1c
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-28 10:26:55 +0200

    Implemented in-place array reversal

--- solution4.2.js
+++ solution4.2.js
@@ -5,3 +5,12 @@ function reverseArray(array) {
     }
     return newArray;
   }
+  function reverseArrayInPlace(array) {
+    for (let i = 0; i < Math.floor(array.length / 2); i++) {
+      let old = array[i];
+      array[i] = array[array.length - 1 - i];
+      array[array.length - 1 - i] = old;
+    }
+    return array;
+  }
+  
