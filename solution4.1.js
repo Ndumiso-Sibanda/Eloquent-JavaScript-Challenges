5fd462abced134247b9cd6a23ec1614a77a05687
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-28 10:17:25 +0200

    Developed range function for creating numeric sequences

---
+++ solution4.1.js
@@ -0,0 +1,8 @@
+function range(start, end, step = 1) {
+    let array = [];
+    if (step > 0) {
+      for (let i = start; i <= end; i += step) array.push(i);
+    } else {
+      for (let i = start; i >= end; i += step) array.push(i);
+    }
+}
